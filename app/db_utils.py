9952e8b12b79824633de820e50c1a9066398aa5f
Devkumar Nirmal <devkumar.nirmal@everestek.com>
2025-09-21 16:09:00 +0530

    removed database for now

--- app/db_utils.py
+++ app/db_utils.py
@@ -1,21 +1,21 @@
-# app/db_utils.py
-from app.db import SessionLocal, Candidate
-
-def save_candidate(name, email, location, job_role, resume_path, jd_path, match_score, matched_keywords, feedback):
-    db = SessionLocal()
-    candidate = Candidate(
-        name=name,
-        email=email,
-        location=location,
-        job_role=job_role,
-        resume_path=str(resume_path),
-        jd_path=str(jd_path),
-        match_score=match_score,
-        matched_keywords=matched_keywords,
-        feedback=feedback
-    )
-    db.add(candidate)
-    db.commit()
-    db.refresh(candidate)
-    db.close()
-    return candidate.id
+# # app/db_utils.py
+# from app.db import SessionLocal, Candidate
+#
+# def save_candidate(name, email, location, job_role, resume_path, jd_path, match_score, matched_keywords, feedback):
+#     db = SessionLocal()
+#     candidate = Candidate(
+#         name=name,
+#         email=email,
+#         location=location,
+#         job_role=job_role,
+#         resume_path=str(resume_path),
+#         jd_path=str(jd_path),
+#         match_score=match_score,
+#         matched_keywords=matched_keywords,
+#         feedback=feedback
+#     )
+#     db.add(candidate)
+#     db.commit()
+#     db.refresh(candidate)
+#     db.close()
+#     return candidate.id
